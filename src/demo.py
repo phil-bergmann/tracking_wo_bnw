01c36c4eef5113972ea5c76a7f4134e04aa2da2d
phil-bergmann <philipp.bergmann@tum.de>
2017-09-27 14:20:20 +0200

    Delete old FasterRCNN

--- src/demo.py
+++
@@ -1,7 +0,0 @@
-def demo_faster_rcnn():
-    from faster_rcnn.solver import Solver
-    frcnn_solver = Solver()
-    frcnn_solver.demo()
-
-if __name__ == '__main__':
-    demo_faster_rcnn()
--- src/train.py
+++
@@ -1,7 +0,0 @@
-def train_faster_rcnn():
-    from faster_rcnn.solver import Solver
-    frcnn_solver = Solver()
-    frcnn_solver.train()
-
-if __name__ == '__main__':
-    train_faster_rcnn()
