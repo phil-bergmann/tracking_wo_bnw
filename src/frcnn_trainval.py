6bb7d0027bda985d556c923458bdc3157bbfd770
phil-bergmann <philipp.bergmann@tum.de>
2017-10-09 16:35:30 +0200

    Not callable anymore and added support for pretrained basenet (eg voc)

--- src/frcnn_trainval.py
+++ src/frcnn_trainval.py
@@ -7,12 +7,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import _init_paths
+import torch
+
+import frcnn
 from model.train_val import get_training_roidb, train_net
 from model.config import cfg, cfg_from_file, cfg_from_list, get_output_dir, get_output_tb_dir
 from datasets.factory import get_imdb
 import datasets.imdb
-import argparse
 import pprint
 import numpy as np
 import sys
@@ -20,43 +21,6 @@ import sys
 from nets.vgg16 import vgg16
 from nets.resnet_v1 import resnetv1
 #from nets.mobilenet_v1 import mobilenetv1
-
-def parse_args():
-  """
-  Parse input arguments
-  """
-  parser = argparse.ArgumentParser(description='Train a Fast R-CNN network')
-  parser.add_argument('--cfg', dest='cfg_file',
-                      help='optional config file',
-                      default=None, type=str)
-  parser.add_argument('--weight', dest='weight',
-                      help='initialize with pretrained model weights',
-                      type=str)
-  parser.add_argument('--imdb', dest='imdb_name',
-                      help='dataset to train on',
-                      default='voc_2007_trainval', type=str)
-  parser.add_argument('--imdbval', dest='imdbval_name',
-                      help='dataset to validate on',
-                      default='voc_2007_test', type=str)
-  parser.add_argument('--iters', dest='max_iters',
-                      help='number of iterations to train',
-                      default=70000, type=int)
-  parser.add_argument('--tag', dest='tag',
-                      help='tag of the model',
-                      default=None, type=str)
-  parser.add_argument('--net', dest='net',
-                      help='vgg16, res50, res101, res152, mobile',
-                      default='res50', type=str)
-  parser.add_argument('--set', dest='set_cfgs',
-                      help='set config keys', default=None,
-                      nargs=argparse.REMAINDER)
-
-  if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
-
-  args = parser.parse_args()
-  return args
 
 
 def combined_roidb(imdb_names):
@@ -84,16 +48,23 @@ def combined_roidb(imdb_names):
   return imdb, roidb
 
 
-if __name__ == '__main__':
-  args = parse_args()
+def frcnn_trainval(args):
+  """
+  args = {'imdb_name':imdb_name,
+      'imdbval_name':imdbval_name,
+      'max_iters':max_iters,
+      'net':network,
+      'cfg_file':cfg_file,
+      'set_cfgs':set_cfgs,
+      'weights':weights,
+      'voc_basenet':voc_basenet,
+      'tag':tag}
+  """
 
-  print('Called with args:')
-  print(args)
-
-  if args.cfg_file is not None:
-    cfg_from_file(args.cfg_file)
-  if args.set_cfgs is not None:
-    cfg_from_list(args.set_cfgs)
+  if args['cfg_file']:
+    cfg_from_file(cfg_file)
+  if args['set_cfgs']:
+    cfg_from_list(set_cfgs)
 
   print('Using config:')
   pprint.pprint(cfg)
@@ -101,38 +72,39 @@ if __name__ == '__main__':
   np.random.seed(cfg.RNG_SEED)
 
   # train set
-  imdb, roidb = combined_roidb(args.imdb_name)
+  imdb, roidb = combined_roidb(args['imdb_name'])
   print('{:d} roidb entries'.format(len(roidb)))
 
   # output directory where the models are saved
-  output_dir = get_output_dir(imdb, args.tag)
+  output_dir = get_output_dir(imdb, args['tag'])
   print('Output will be saved to `{:s}`'.format(output_dir))
 
   # tensorboard directory where the summaries are saved during training
-  tb_dir = get_output_tb_dir(imdb, args.tag)
+  tb_dir = get_output_tb_dir(imdb, args['tag'])
   print('TensorFlow summaries will be saved to `{:s}`'.format(tb_dir))
 
   # also add the validation set, but with no flipping images
   orgflip = cfg.TRAIN.USE_FLIPPED
   cfg.TRAIN.USE_FLIPPED = False
-  _, valroidb = combined_roidb(args.imdbval_name)
+  _, valroidb = combined_roidb(args['imdbval_name'])
   print('{:d} validation roidb entries'.format(len(valroidb)))
   cfg.TRAIN.USE_FLIPPED = orgflip
 
   # load network
-  if args.net == 'vgg16':
+  if args['net'] == 'vgg16':
     net = vgg16(batch_size=cfg.TRAIN.IMS_PER_BATCH)
-  elif args.net == 'res50':
+  elif args['net'] == 'res50':
     net = resnetv1(batch_size=cfg.TRAIN.IMS_PER_BATCH, num_layers=50)
-  elif args.net == 'res101':
+  elif args['net'] == 'res101':
     net = resnetv1(batch_size=cfg.TRAIN.IMS_PER_BATCH, num_layers=101)
-  elif args.net == 'res152':
+  elif args['net'] == 'res152':
     net = resnetv1(batch_size=cfg.TRAIN.IMS_PER_BATCH, num_layers=152)
-  elif args.net == 'mobile':
+  elif args['net'] == 'mobile':
     net = mobilenetv1(batch_size=cfg.TRAIN.IMS_PER_BATCH)
   else:
     raise NotImplementedError
+
     
   train_net(net, imdb, roidb, valroidb, output_dir, tb_dir,
-            pretrained_model=args.weight,
-            max_iters=args.max_iters)
+            pretrained_model=args['weights'],
+            max_iters=args['max_iters'], basenet=args['basenet'])
