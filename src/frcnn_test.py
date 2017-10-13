6d69d75bf522721cec7e668d79ddfc80da536341
phil-bergmann <philipp.bergmann@tum.de>
2017-10-13 17:31:39 +0200

    not callable anymore, will be imported by experiment scripts

--- src/frcnn_test.py
+++ src/frcnn_test.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import _init_paths
+import frcnn
 from model.test import test_net
 from model.config import cfg, cfg_from_file, cfg_from_list
 from datasets.factory import get_imdb
@@ -21,97 +21,57 @@ from nets.resnet_v1 import resnetv1
 
 import torch
 
-def parse_args():
-  """
-  Parse input arguments
-  """
-  parser = argparse.ArgumentParser(description='Test a Fast R-CNN network')
-  parser.add_argument('--cfg', dest='cfg_file',
-            help='optional config file', default=None, type=str)
-  parser.add_argument('--model', dest='model',
-            help='model to test',
-            default=None, type=str)
-  parser.add_argument('--imdb', dest='imdb_name',
-            help='dataset to test',
-            default='voc_2007_test', type=str)
-  parser.add_argument('--comp', dest='comp_mode', help='competition mode',
-            action='store_true')
-  parser.add_argument('--num_dets', dest='max_per_image',
-            help='max number of detections per image',
-            default=100, type=int)
-  parser.add_argument('--tag', dest='tag',
-                        help='tag of the model',
-                        default='', type=str)
-  parser.add_argument('--net', dest='net',
-                      help='vgg16, res50, res101, res152, mobile',
-                      default='res50', type=str)
-  parser.add_argument('--set', dest='set_cfgs',
-                        help='set config keys', default=None,
-                        nargs=argparse.REMAINDER)
+def frcnn_test(args):
+    """
+    args = {#'imdb_name':imdb_name,
+            'imdbtest_name':imdbtest_name,
+            'net':network,
+            'cfg_file':None,
+            'set_cfgs':None,
+            'tag':tag,
+            'comp_mode':comp,
+            'max_per_image':max_per_image,
+            'output_dir':output_dir,
+            'model':model}
+    """
 
-  if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
+    if args['cfg_file']:
+        cfg_from_file(args['cfg_file'])
+    if args['set_cfgs']:
+        cfg_from_list(args['set_cfgs'])
 
-  args = parser.parse_args()
-  return args
+    print('Using config:')
+    pprint.pprint(cfg)
 
-if __name__ == '__main__':
-  args = parse_args()
+    #filename = os.path.splitext(os.path.basename(args['model']))[0]
 
-  print('Called with args:')
-  print(args)
+    imdb_test = get_imdb(args['imdbtest_name'])
+    imdb_test.competition_mode(args['comp_mode'])
 
-  if args.cfg_file is not None:
-    cfg_from_file(args.cfg_file)
-  if args.set_cfgs is not None:
-    cfg_from_list(args.set_cfgs)
+    # load network
+    if args['net'] == 'vgg16':
+        net = vgg16(batch_size=1)
+    elif args['net'] == 'res50':
+        net = resnetv1(batch_size=1, num_layers=50)
+    elif args['net'] == 'res101':
+        net = resnetv1(batch_size=1, num_layers=101)
+    elif args['net'] == 'res152':
+        net = resnetv1(batch_size=1, num_layers=152)
+    else:
+        raise NotImplementedError
 
-  print('Using config:')
-  pprint.pprint(cfg)
+    # load model
+    net.create_architecture(imdb_test.num_classes, tag='default',
+        anchor_scales=cfg.ANCHOR_SCALES,
+        anchor_ratios=cfg.ANCHOR_RATIOS)
 
-  # if has model, get the name from it
-  # if does not, then just use the inialization weights
-  if args.model:
-    filename = os.path.splitext(os.path.basename(args.model))[0]
-  else:
-    filename = os.path.splitext(os.path.basename(args.weight))[0]
+    net.eval()
+    net.cuda()
 
-  tag = args.tag
-  tag = tag if tag else 'default'
-  filename = tag + '/' + filename
 
-  imdb = get_imdb(args.imdb_name)
-  imdb.competition_mode(args.comp_mode)
-
-  # load network
-  if args.net == 'vgg16':
-    net = vgg16(batch_size=1)
-  elif args.net == 'res50':
-    net = resnetv1(batch_size=1, num_layers=50)
-  elif args.net == 'res101':
-    net = resnetv1(batch_size=1, num_layers=101)
-  elif args.net == 'res152':
-    net = resnetv1(batch_size=1, num_layers=152)
-  elif args.net == 'mobile':
-    net = mobilenetv1(batch_size=1)
-  else:
-    raise NotImplementedError
-
-  # load model
-  net.create_architecture(imdb.num_classes, tag='default',
-                          anchor_scales=cfg.ANCHOR_SCALES,
-                          anchor_ratios=cfg.ANCHOR_RATIOS)
-
-  net.eval()
-  net.cuda()
-
-  if args.model:
-    print(('Loading model check point from {:s}').format(args.model))
-    net.load_state_dict(torch.load(args.model))
-    print('Loaded.')
-  else:
-    print(('Loading initial weights from {:s}').format(args.weight))
+    print(('Loading model check point from {:s}').format(args['model']))
+    net.load_state_dict(torch.load(args['model']))
     print('Loaded.')
 
-  test_net(net, imdb, filename, max_per_image=args.max_per_image)
+
+    test_net(net, imdb_test, args['output_dir'], max_per_image=args['max_per_image'])
