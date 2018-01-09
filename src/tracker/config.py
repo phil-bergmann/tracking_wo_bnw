809048d6dc12c7cfea2f5ff69a1fe799f1751caa
Philipp Bergmann <bergmanp@hpccremers4>
2018-01-09 14:33:35 +0100

    more entries and utility functions

--- src/tracker/config.py
+++ src/tracker/config.py
@@ -37,7 +37,10 @@ __C.TRAIN.SMP_PER_BATCH = 1
 __C.TRAIN.SNAPSHOT_PREFIX = "LSTM_v0.1"
 
 # Make Snapshot every Iters
-__C.TRAIN.SNAPSHOT_ITERS = 1000
+__C.TRAIN.SNAPSHOT_ITERS = 100000
+
+# Make Image every Iters
+__C.TRAIN.IMAGE_ITERS = 1000
 
 
 #
@@ -57,7 +60,11 @@ __C.LSTM.HIDDEN_NUM = 500
 # number of layers
 __C.LSTM.LAYERS = 1
 
+# downsample to 300xN
+__C.LSTM.SAMPLE_N = 20
 
+# permute inputs
+__C.LSTM.PERM_INP = False
 
 
 # For reproducibility
@@ -103,4 +110,72 @@ def get_tb_dir(module):
     os.makedirs(outdir)
   return outdir
 
+def get_cache_dir():
+  outdir = osp.abspath(osp.join(__C.ROOT_DIR, 'output', 'cache'))
+  if not os.path.exists(outdir):
+    os.makedirs(outdir)
+  return outdir
 
+def _merge_a_into_b(a, b):
+  """Merge config dictionary a into config dictionary b, clobbering the
+  options in b whenever they are also specified in a.
+  """
+  if type(a) is not edict:
+    return
+
+  for k, v in a.items():
+    # a must specify keys that are in b
+    if k not in b:
+      raise KeyError('{} is not a valid config key'.format(k))
+
+    # the types must match, too
+    old_type = type(b[k])
+    if old_type is not type(v):
+      if isinstance(b[k], np.ndarray):
+        v = np.array(v, dtype=b[k].dtype)
+      else:
+        raise ValueError(('Type mismatch ({} vs. {}) '
+                          'for config key: {}').format(type(b[k]),
+                                                       type(v), k))
+
+    # recursively merge dicts
+    if type(v) is edict:
+      try:
+        _merge_a_into_b(a[k], b[k])
+      except:
+        print(('Error under config key: {}'.format(k)))
+        raise
+    else:
+      b[k] = v
+
+
+def cfg_from_file(filename):
+  """Load a config file and merge it into the default options."""
+  import yaml
+  with open(filename, 'r') as f:
+    yaml_cfg = edict(yaml.load(f))
+
+  _merge_a_into_b(yaml_cfg, __C)
+
+
+def cfg_from_list(cfg_list):
+  """Set config keys via list (e.g., from command line)."""
+  from ast import literal_eval
+  assert len(cfg_list) % 2 == 0
+  for k, v in zip(cfg_list[0::2], cfg_list[1::2]):
+    key_list = k.split('.')
+    d = __C
+    for subkey in key_list[:-1]:
+      assert subkey in d
+      d = d[subkey]
+    subkey = key_list[-1]
+    assert subkey in d
+    try:
+      value = literal_eval(v)
+    except:
+      # handle the case when v is a string literal
+      value = v
+    assert type(value) == type(d[subkey]), \
+      'type {} does not match original type {}'.format(
+        type(value), type(d[subkey]))
+    d[subkey] = value
