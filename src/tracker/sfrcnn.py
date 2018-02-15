a05f7367940ac7acca027427967d3124344c9a84
phil-bergmann <philipp.bergmann@tum.de>
2018-02-15 16:48:32 +0100

    interface changes and bug fixes

--- src/tracker/sfrcnn.py
+++ src/tracker/sfrcnn.py
@@ -14,17 +14,22 @@ import torch.nn.functional as F
 class FRCNN(vgg16):
 	
 	
-	def _predict(self, rois=None):
+	def _predict(self, rois, image_to_head):
 		# This is just _build_network in tf-faster-rcnn
 		torch.backends.cudnn.benchmark = False
-		net_conv = self._image_to_head()
 
-		# build the anchors for the image
-		self._anchor_component(net_conv.size(2), net_conv.size(3))
+		if image_to_head:
+			net_conv = self._image_to_head()
+			self._net_conv = net_conv
+		else:
+			net_conv = self._net_conv
 
 		if not hasattr(rois, 'size'):
+			# build the anchors for the image
+			self._anchor_component(net_conv.size(2), net_conv.size(3))
 			rois = self._region_proposal(net_conv)
 		else:
+			rois = Variable(bb2rois(rois)).cuda()
 			self._predictions["rois"] = rois
 
 		if frcnn_cfg.POOLING_MODE == 'crop':
@@ -46,22 +51,26 @@ class FRCNN(vgg16):
 		return rois, cls_prob, bbox_pred
 
 	def get_rois(self):
-		return self._predictions["rois"]
+		return self._predictions["rois"].data
 
 	def get_fc7(self):
-		return self._predictions["fc7"].data.cpu().numpy()
+		return self._predictions["fc7"].data
 
-	def forward(self, image, im_info, rois=None, mode='TEST'):
+	def forward(self, image, im_info, rois, mode='TEST'):
 		#self._image_gt_summaries['image'] = image
 		#self._image_gt_summaries['gt_boxes'] = gt_boxes
 		#self._image_gt_summaries['im_info'] = im_info
 
-		self._image = Variable(torch.from_numpy(image.transpose([0,3,1,2])).cuda(), volatile=mode == 'TEST')
-		self._im_info = im_info # No need to change; actually it can be an list
+		# should be changed later but for now it is ok
+		image_to_head = False
+		if hasattr(image, 'size'):
+			self._image = Variable(torch.from_numpy(image.cpu().numpy().transpose([0,3,1,2])).cuda(), volatile=mode == 'TEST')
+			self._im_info = im_info.cpu().numpy() # No need to change; actually it can be an list
+			image_to_head = True
 
 		self._mode = mode
 
-		rois, cls_prob, bbox_pred = self._predict(rois)
+		rois, cls_prob, bbox_pred = self._predict(rois, image_to_head)
 
 		stds = bbox_pred.data.new(frcnn_cfg.TRAIN.BBOX_NORMALIZE_STDS).repeat(self._num_classes).unsqueeze(0).expand_as(bbox_pred)
 		means = bbox_pred.data.new(frcnn_cfg.TRAIN.BBOX_NORMALIZE_MEANS).repeat(self._num_classes).unsqueeze(0).expand_as(bbox_pred)
@@ -70,11 +79,23 @@ class FRCNN(vgg16):
 	def test_image(self, image, im_info, rois=None):
 		self.eval()
 		self.forward(image, im_info, rois)
-		cls_score, cls_prob, bbox_pred, rois = self._predictions["cls_score"].data.cpu().numpy(), \
-													 self._predictions['cls_prob'].data.cpu().numpy(), \
-													 self._predictions['bbox_pred'].data.cpu().numpy(), \
-													 self._predictions['rois'].data.cpu().numpy()
+		cls_score, cls_prob, bbox_pred, rois = self._predictions["cls_score"].data, \
+													 self._predictions['cls_prob'].data, \
+													 self._predictions['bbox_pred'].data, \
+													 self._predictions['rois'].data[:,1:5]
 		return cls_score, cls_prob, bbox_pred, rois
 
+	def test_rois(self, rois):
+		return self.test_image(None, None, rois)
 
+def boxes2rois(boxes, cl=1):
+	rois_score = boxes.new(boxes.size()[0],1).zero_()
+	rois_bb = boxes[:, cl*4:(cl+1)*4]
+	rois = torch.cat((rois_score, rois_bb),1)
 
+	return rois
+
+def bb2rois(bb):
+	rois_score = bb.new(bb.size()[0],1).zero_()
+	rois = torch.cat((rois_score, bb), 1)
+	return rois
