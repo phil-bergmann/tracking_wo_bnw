7c7c9cd40520186b219dbf3bd27e4967ffc54f33
Tim Meinhardt <meinhardt.tim@gmail.com>
2020-04-20 14:09:48 +0200

    Fix oracle trackers

--- src/tracktor/oracle_tracker.py
+++ src/tracktor/oracle_tracker.py
@@ -3,10 +3,8 @@ import torch
 from torch.autograd import Variable
 from scipy.optimize import linear_sum_assignment
 
-from .utils import bbox_transform_inv, clip_boxes
+from torchvision.ops.boxes import clip_boxes_to_image, nms
 
-# from frcnn.model.bbox_transform import bbox_transform_inv, clip_boxes
-# from frcnn.model.nms_wrapper import nms
 from .tracker import Tracker
 from .utils import bbox_overlaps
 
@@ -56,11 +54,8 @@ class OracleTracker(Tracker):
 		pos = self.get_pos()
 
 		# regress
-		_, scores, bbox_pred, rois = self.obj_detect.test_rois(pos)
-		boxes = bbox_transform_inv(rois, bbox_pred)
-		boxes = clip_boxes(Variable(boxes), blob['im_info'][0][:2]).data
-		pos = boxes[:, self.cl * 4:(self.cl + 1) * 4]
-		scores = scores[:, self.cl]
+		boxes, scores = self.obj_detect.predict_boxes(pos)
+		pos = clip_boxes_to_image(boxes, blob['img'].shape[-2:])
 
 		s = []
 		for i in range(len(self.tracks) - 1, -1, -1):
@@ -77,111 +72,116 @@ class OracleTracker(Tracker):
 		return torch.Tensor(s[::-1]).cuda()
 
 	def reid(self, blob, new_det_pos, new_det_scores):
-		new_det_features = self.reid_network.test_rois(blob['app_data'][0], new_det_pos / blob['im_info'][0][2]).data
-		if len(self.inactive_tracks) >= 1 and self.do_reid:
-			# calculate appearance distances
-			dist_mat = []
-			pos = []
-			for t in self.inactive_tracks:
-				dist_mat.append(torch.cat([t.test_features(feat.view(1, -1)) for feat in new_det_features], 1))
-				pos.append(t.pos)
-			if len(dist_mat) > 1:
-				dist_mat = torch.cat(dist_mat, 0)
-				pos = torch.cat(pos, 0)
-			else:
-				dist_mat = dist_mat[0]
-				pos = pos[0]
+		new_det_features = [torch.zeros(0).cuda() for _ in range(len(new_det_pos))]
 
-			# calculate IoU distances
-			iou = bbox_overlaps(pos, new_det_pos)
-			iou_mask = torch.ge(iou, self.reid_iou_threshold)
-			iou_neg_mask = ~iou_mask
-			# make all impossible assignments to the same add big value
-			dist_mat = dist_mat * iou_mask.float() + iou_neg_mask.float() * 1000
-			dist_mat = dist_mat.cpu().numpy()
+		if self.do_reid:
+			new_det_features = self.reid_network.test_rois(
+				blob['img'], new_det_pos).data
 
-			row_ind, col_ind = linear_sum_assignment(dist_mat)
+			if len(self.inactive_tracks) >= 1:
+				# calculate appearance distances
+				dist_mat, pos = [], []
+				for t in self.inactive_tracks:
+					dist_mat.append(torch.cat([t.test_features(feat.view(1, -1))
+					                           for feat in new_det_features], dim=1))
+					pos.append(t.pos)
+				if len(dist_mat) > 1:
+					dist_mat = torch.cat(dist_mat, 0)
+					pos = torch.cat(pos, 0)
+				else:
+					dist_mat = dist_mat[0]
+					pos = pos[0]
 
-			assigned = []
-			remove_inactive = []
-			for r,c in zip(row_ind, col_ind):
-				if dist_mat[r,c] <= self.reid_sim_threshold:
-					###### ADD GT ID ######
-					gt = blob['gt']
-					boxes = torch.cat(list(gt.values()), 0).cuda()
-					# boxes = clip_boxes(Variable(boxes), blob['im_info'][0][:2]).data
-					tracks_iou = bbox_overlaps(t.pos, boxes).cpu().numpy()
-					ind = np.where(tracks_iou==np.max(tracks_iou))[1]
-					if len(ind) > 0:
-						ind = ind[0]
-						overlap = tracks_iou[0,ind]
-						if overlap >= 0.5:
-							gt_id = list(gt.keys())[ind]
-							t.gt_id = gt_id
-							if self.pos_oracle:
-								t.pos = gt[gt_id].cuda()
-						elif self.kill_oracle:
-							continue
-					t = self.inactive_tracks[r]
-					self.tracks.append(t)
-					t.count_inactive = 0
-					t.reset_last_pos()
-					t.pos = new_det_pos[c].view(1,-1)
-					t.add_features(new_det_features[c].view(1,-1))
-					assigned.append(c)
-					remove_inactive.append(t)
+				# calculate IoU distances
+				iou = bbox_overlaps(pos, new_det_pos)
+				iou_mask = torch.ge(iou, self.reid_iou_threshold)
+				iou_neg_mask = ~iou_mask
+				# make all impossible assignments to the same add big value
+				dist_mat = dist_mat * iou_mask.float() + iou_neg_mask.float() * 1000
+				dist_mat = dist_mat.cpu().numpy()
 
-			for t in remove_inactive:
-				self.inactive_tracks.remove(t)
+				row_ind, col_ind = linear_sum_assignment(dist_mat)
 
-			keep = torch.Tensor([i for i in range(new_det_pos.size(0)) if i not in assigned]).long().cuda()
-			if keep.nelement() > 0:
-				new_det_pos = new_det_pos[keep]
-				new_det_scores = new_det_scores[keep]
-				new_det_features = new_det_features[keep]
-			else:
-				new_det_pos = torch.zeros(0).cuda()
-				new_det_scores = torch.zeros(0).cuda()
-				new_det_features = torch.zeros(0).cuda()
+				assigned = []
+				remove_inactive = []
+				for r,c in zip(row_ind, col_ind):
+					if dist_mat[r,c] <= self.reid_sim_threshold:
+						###### ADD GT ID ######
+						gt = blob['gt']
+						boxes = torch.cat(list(gt.values()), 0).cuda()
+						# boxes = clip_boxes(Variable(boxes), blob['im_info'][0][:2]).data
+						tracks_iou = bbox_overlaps(t.pos, boxes).cpu().numpy()
+						ind = np.where(tracks_iou==np.max(tracks_iou))[1]
+						if len(ind) > 0:
+							ind = ind[0]
+							overlap = tracks_iou[0,ind]
+							if overlap >= 0.5:
+								gt_id = list(gt.keys())[ind]
+								t.gt_id = gt_id
+								if self.pos_oracle:
+									t.pos = gt[gt_id].cuda()
+							elif self.kill_oracle:
+								continue
+						t = self.inactive_tracks[r]
+						self.tracks.append(t)
+						t.count_inactive = 0
+						t.reset_last_pos()
+						t.pos = new_det_pos[c].view(1,-1)
+						t.add_features(new_det_features[c].view(1,-1))
+						assigned.append(c)
+						remove_inactive.append(t)
 
-		if len(self.inactive_tracks) >= 1 and self.reid_oracle:
-			gt = blob['gt']
-			gt_pos = torch.cat(list(gt.values()), 0).cuda()
-			gt_ids = list(gt.keys())
+				for t in remove_inactive:
+					self.inactive_tracks.remove(t)
 
-			# calculate IoU distances
-			iou_neg = 1 - bbox_overlaps(new_det_pos, gt_pos)
-			dist_mat = iou_neg.cpu().numpy()
+				keep = torch.Tensor([i for i in range(new_det_pos.size(0)) if i not in assigned]).long().cuda()
+				if keep.nelement() > 0:
+					new_det_pos = new_det_pos[keep]
+					new_det_scores = new_det_scores[keep]
+					new_det_features = new_det_features[keep]
+				else:
+					new_det_pos = torch.zeros(0).cuda()
+					new_det_scores = torch.zeros(0).cuda()
+					new_det_features = torch.zeros(0).cuda()
 
-			row_ind, col_ind = linear_sum_assignment(dist_mat)
+			if len(self.inactive_tracks) >= 1 and self.reid_oracle:
+				gt = blob['gt']
+				gt_pos = torch.cat(list(gt.values()), 0).cuda()
+				gt_ids = list(gt.keys())
 
-			assigned = []
-			for r, c in zip(row_ind, col_ind):
-				if dist_mat[r, c] <= 0.5:
-					gt_id = gt_ids[c]
+				# calculate IoU distances
+				iou_neg = 1 - bbox_overlaps(new_det_pos, gt_pos)
+				dist_mat = iou_neg.cpu().numpy()
 
-					# loop through inactive in inversed order to get newest dead track
-					for i in range(len(self.inactive_tracks) - 1, -1, -1):
-						t = self.inactive_tracks[i]
-						if t.gt_id == gt_id:
-							if self.pos_oracle:
-								t.pos = gt_pos[c].view(1, -1)
-							else:
-								t.pos = new_det_pos[r, :].view(1, -1)
-							self.inactive_tracks.remove(t)
-							self.tracks.append(t)
-							t.reset_last_pos()
-							assigned.append(r)
+				row_ind, col_ind = linear_sum_assignment(dist_mat)
 
-			keep = torch.Tensor([i for i in range(new_det_pos.size(0)) if i not in assigned]).long().cuda()
-			if keep.nelement() > 0:
-				new_det_pos = new_det_pos[keep]
-				new_det_scores = new_det_scores[keep]
-				new_det_features = new_det_features[keep]
-			else:
-				new_det_pos = torch.zeros(0).cuda()
-				new_det_scores = torch.zeros(0).cuda()
-				new_det_features = torch.zeros(0).cuda()
+				assigned = []
+				for r, c in zip(row_ind, col_ind):
+					if dist_mat[r, c] <= 0.5:
+						gt_id = gt_ids[c]
+
+						# loop through inactive in inversed order to get newest dead track
+						for i in range(len(self.inactive_tracks) - 1, -1, -1):
+							t = self.inactive_tracks[i]
+							if t.gt_id == gt_id:
+								if self.pos_oracle:
+									t.pos = gt_pos[c].view(1, -1)
+								else:
+									t.pos = new_det_pos[r, :].view(1, -1)
+								self.inactive_tracks.remove(t)
+								self.tracks.append(t)
+								t.reset_last_pos()
+								assigned.append(r)
+
+				keep = torch.Tensor([i for i in range(new_det_pos.size(0)) if i not in assigned]).long().cuda()
+				if keep.nelement() > 0:
+					new_det_pos = new_det_pos[keep]
+					new_det_scores = new_det_scores[keep]
+					new_det_features = new_det_features[keep]
+				else:
+					new_det_pos = torch.zeros(0).cuda()
+					new_det_scores = torch.zeros(0).cuda()
+					new_det_features = torch.zeros(0).cuda()
 
 		return new_det_pos, new_det_scores, new_det_features
 
@@ -206,7 +206,7 @@ class OracleTracker(Tracker):
 						ht = y2t - y1t
 
 						# extract coordinates of current pos
-						new_pos = clip_boxes(Variable(new_pos), blob['im_info'][0][:2]).data
+						new_pos = clip_boxes_to_image(new_pos, blob['img'].shape[-2:])
 						x1n = new_pos[0, 0]
 						y1n = new_pos[0, 1]
 						x2n = new_pos[0, 2]
@@ -224,7 +224,7 @@ class OracleTracker(Tracker):
 
 		# now take care that all tracks are inside the image (normally done by regress)
 		for t in self.tracks:
-			t.pos = clip_boxes(Variable(t.pos), blob['im_info'][0][:2]).data
+			t.pos = clip_boxes_to_image(t.pos, blob['img'].shape[-2:])
 
 		if len(self.tracks):
 			pos = self.get_pos()
@@ -354,22 +354,22 @@ class OracleTracker(Tracker):
 		# Look for new detections #
 		###########################
 
-		self.obj_detect.load_image(blob['data'][0], blob['im_info'][0])
+		self.obj_detect.load_image(blob['img'])
+
 		if self.public_detections:
-			dets = blob['dets']
-			if len(dets) > 0:
-				dets = torch.cat(dets, 0)[:, :4]
-				_, scores, bbox_pred, rois = self.obj_detect.test_rois(dets)
+			dets = blob['dets'].squeeze(dim=0)
+			if dets.nelement() > 0:
+				boxes, scores = self.obj_detect.predict_boxes(dets)
 			else:
-				rois = torch.zeros(0).cuda()
+				boxes = scores = torch.zeros(0).cuda()
 		else:
-			_, scores, bbox_pred, rois = self.obj_detect.detect()
+			boxes, scores = self.obj_detect.detect(blob['img'])
 
-		if rois.nelement() > 0:
-			boxes = bbox_transform_inv(rois, bbox_pred)
-			boxes = clip_boxes(Variable(boxes), blob['im_info'][0][:2]).data
+		if boxes.nelement() > 0:
+			boxes = clip_boxes_to_image(boxes, blob['img'].shape[-2:])
 
-			scores = scores[:, self.cl]
+			# Filter out tracks that have too low person score
+			inds = torch.gt(scores, self.detection_person_thresh).nonzero().view(-1)
 
 			if self.kill_oracle:
 				gt = blob['gt']
@@ -395,8 +395,8 @@ class OracleTracker(Tracker):
 			inds = torch.zeros(0).cuda()
 
 		if inds.nelement() > 0:
-			boxes = boxes[inds]
-			det_pos = boxes[:, self.cl * 4:(self.cl + 1) * 4]
+			det_pos = boxes[inds]
+
 			det_scores = scores[inds]
 		else:
 			det_pos = torch.zeros(0).cuda()
@@ -430,7 +430,7 @@ class OracleTracker(Tracker):
 
 			if len(self.tracks):
 				# create nms input
-				new_features = self.get_appearances(blob)
+
 
 				# nms here if tracks overlap
 				nms_inp_reg = torch.cat((self.get_pos(), person_scores.add_(3).view(-1, 1)), 1)
@@ -443,40 +443,38 @@ class OracleTracker(Tracker):
 				# Plot the killed tracks for debugging
 				self.tracks_to_inactive([self.tracks[i] for i in list(range(len(self.tracks))) if i not in keep])
 
-				if keep.nelement() > 0:
+				if keep.nelement() > 0 and self.do_reid:
 					nms_inp_reg = nms_inp_reg[keep]
+					new_features = self.get_appearances(blob)
 					self.add_features(new_features)
 					num_tracks = nms_inp_reg.size(0)
-				else:
-					nms_inp_reg = torch.zeros(0).cuda()
-					num_tracks = 0
 
 		#####################
 		# Create new tracks #
 		#####################
 
-		# create nms input and nms new detections
 		if det_pos.nelement() > 0:
-			nms_inp_det = torch.cat((det_pos, det_scores.view(-1, 1)), 1)
-		else:
-			nms_inp_det = torch.zeros(0).cuda()
-		if nms_inp_det.nelement() > 0:
-			keep = nms(nms_inp_det, self.detection_nms_thresh)
-			nms_inp_det = nms_inp_det[keep]
+			keep = nms(det_pos, det_scores, self.detection_nms_thresh)
+			det_pos = det_pos[keep]
+			det_scores = det_scores[keep]
 
 			# check with every track in a single run (problem if tracks delete each other)
-			for i in range(num_tracks):
-				nms_inp = torch.cat((nms_inp_reg[i].view(1, -1), nms_inp_det), 0)
-				keep = nms(nms_inp, self.detection_nms_thresh)
-				keep = keep[torch.ge(keep, 1)]
-				if keep.nelement() == 0:
-					nms_inp_det = nms_inp_det.new(0)
-					break
-				nms_inp_det = nms_inp[keep]
+			for t in self.tracks:
+				nms_track_pos = torch.cat([t.pos, det_pos])
+				nms_track_scores = torch.cat(
+					[torch.tensor([2.0]).to(det_scores.device), det_scores])
+				keep = nms(nms_track_pos, nms_track_scores, self.detection_nms_thresh)
 
-		if nms_inp_det.nelement() > 0:
-			new_det_pos = nms_inp_det[:, :4]
-			new_det_scores = nms_inp_det[:, 4]
+				keep = keep[torch.ge(keep, 1)] - 1
+
+				det_pos = det_pos[keep]
+				det_scores = det_scores[keep]
+				if keep.nelement() == 0:
+					break
+
+		if det_pos.nelement() > 0:
+			new_det_pos = det_pos
+			new_det_scores = det_scores
 
 			# try to reidentify tracks
 			new_det_pos, new_det_scores, new_det_features = self.reid(blob, new_det_pos, new_det_scores)
@@ -490,12 +488,10 @@ class OracleTracker(Tracker):
 		####################
 
 		for t in self.tracks:
-			track_ind = int(t.id)
-			if track_ind not in self.results.keys():
-				self.results[track_ind] = {}
-			pos = t.pos[0] / blob['im_info'][0][2]
-			sc = t.score
-			self.results[track_ind][self.im_index] = np.concatenate([pos.cpu().numpy(), np.array([sc])])
+			if t.id not in self.results.keys():
+				self.results[t.id] = {}
+			self.results[t.id][self.im_index] = np.concatenate(
+				[t.pos[0].cpu().numpy(), np.array([t.score])])
 
 		for t in self.inactive_tracks:
 			t.count_inactive += 1
@@ -506,4 +502,4 @@ class OracleTracker(Tracker):
 			]
 
 		self.im_index += 1
-		self.last_image = blob['data'][0][0]
+		self.last_image = blob['img'][0]
